c92ed4329e2aa38af8b8f19b6009956f45e4acd7
Nisarg Mehta <nisarg@carrot.co>
2018-01-10 16:17:09 -0800

    only have take photo and select from lib options

--- Code/Controllers/ATLConversationViewController.m
+++ Code/Controllers/ATLConversationViewController.m
@@ -608,13 +608,13 @@ static NSInteger const ATLPhotoActionSheet = 1000;
                                                   delegate:self
                                          cancelButtonTitle:ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.cancel.key", @"Cancel", nil)
                                     destructiveButtonTitle:nil
-                                         otherButtonTitles:ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.takephoto.key", @"Take Photo/Video", nil), ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.lastphoto.key", @"Last Photo/Video", nil), nil];
+                                         otherButtonTitles:ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.takephoto.key", @"Take Photo/Video", nil), nil];
     } else {
         actionSheet = [[UIActionSheet alloc] initWithTitle:nil
                                                   delegate:self
                                          cancelButtonTitle:ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.cancel.key", @"Cancel", nil)
                                     destructiveButtonTitle:nil
-                                         otherButtonTitles:ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.takephoto.key", @"Take Photo/Video", nil), ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.lastphoto.key", @"Last Photo/Video", nil), ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.library.key", @"Photo/Video Library", nil), nil];
+                                         otherButtonTitles:ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.takephoto.key", @"Take Photo/Video", nil), ATLLocalizedString(@"atl.conversation.toolbar.actionsheet.library.key", @"Photo/Video Library", nil), nil];
     }
     
     [actionSheet showInView:self.view];
@@ -744,11 +744,11 @@ static NSInteger const ATLPhotoActionSheet = 1000;
                 break;
                 
             case 1:
-                [self captureLastPhotoTaken];
-                break;
-                
-            case 2:
-                [self displayImagePickerWithSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+                if (@available(iOS 11.0, *)) {
+                    
+                } else {
+                    [self displayImagePickerWithSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+                }
                 break;
                 
             default:
